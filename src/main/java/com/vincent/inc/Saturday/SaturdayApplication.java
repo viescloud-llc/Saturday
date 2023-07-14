730d5a66aea228b5dedcfb3099c4579550c4e35e
VincentB00 <vincentbui1999@gmail.com>
2023-07-14 16:00:51 -0400

    init setup

--- src/main/java/com/vincent/inc/Saturday/SaturdayApplication.java
+++ src/main/java/com/vincent/inc/Saturday/SaturdayApplication.java
@@ -1,13 +1,33 @@
 package com.vincent.inc.Saturday;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class SaturdayApplication {
+
+	@Value("${spring.profiles.active}")
+	private String env = "?";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaturdayApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public ModelAndView redirectSwagger(ModelMap model) {
+		return new ModelAndView("redirect:/swagger-ui/index.html", model);
+	}
+
+	@GetMapping("/_status/healthz")
+	public String healthCheck() {
+		return String.format("Authenticator %s is up and running", env);
+	}
 }
