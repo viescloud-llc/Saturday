daa7c98fc500edb6d7b1f67c1df24766a48afa0d
VincentB00 <vincentbui1999@gmail.com>
2023-07-14 15:41:42 -0400

    init

---
+++ src/main/java/com/vincent/inc/Saturday/SaturdayApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.inc.Saturday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaturdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaturdayApplication.class, args);
+	}
+
+}
