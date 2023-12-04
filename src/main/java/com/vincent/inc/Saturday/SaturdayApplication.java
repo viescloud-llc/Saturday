da19a7191532cbe61cee746c0ba108075dbfeeee
VincentB00 <vincentbui1999@gmail.com>
2023-12-04 15:35:50 -0500

    fix pom and main application class

--- src/main/java/com/vincent/inc/Saturday/SaturdayApplication.java
+++ src/main/java/com/vincent/inc/Saturday/SaturdayApplication.java
@@ -1,35 +1,24 @@
 package com.vincent.inc.Saturday;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import com.vincent.inc.viesspringutils.Application;
 
-@SpringBootApplication(scanBasePackages = "com.vincent.inc")
+@SpringBootApplication
 @EnableDiscoveryClient
 @RestController
 @EnableFeignClients
-public class SaturdayApplication {
-
-	@Value("${spring.profiles.active}")
-	private String env = "?";
+public class SaturdayApplication extends Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaturdayApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public ModelAndView redirectSwagger(ModelMap model) {
-		return new ModelAndView("redirect:/swagger-ui/index.html", model);
-	}
-
-	@GetMapping("/_status/healthz")
-	public String healthCheck() {
-		return String.format("Saturday %s is up and running", env);
+	@Override
+	public String getApplicationName() {
+		return "Saturday";
 	}
 }
